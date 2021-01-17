4cb372c11f2272cf190fbc81546ba2a1722edf68
EC2 Default User <ec2-user@ip-172-31-10-39.ap-northeast-1.compute.internal>
2021-01-17 04:44:21 +0000

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,49 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @answer = Answer.new
+  end
+
+  def new
+    @question = Question.new
+  end
+  
+  def create 
+    @question =Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+      render :new
+    end 
+  end  
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to root_path, notice: 'Success'
+    else
+      flash[:aleat] = 'Save error!'
+      render :edit
+    end  
+  end
+  
+  def destroy 
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path, notice: 'Success'
+  end
+  private 
+    def question_params
+      params.require(:question).permit(:name, :title, :content)
+    end  
+      
+end
---
+++ app/models/question.rb
@@ -0,0 +1,8 @@
+class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+end
+ 
